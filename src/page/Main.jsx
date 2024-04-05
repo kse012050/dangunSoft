317f1b0c971e885ada3e2fe31c95f1505c89405b
김성은 <kse012050@naver.com>
2024-04-05 17:10:35 +0900

    메인 - welcome 완료

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <div>
-            sss
-        </div>
+        <>
+            <section className='welcomeArea contentSize'>
+                <h2>
+                    Welcome to the <br className='pc_tablet'/>
+                    NATTOSYSTEM
+                </h2>
+                <p>
+                    NATTOSYSTEM은 JetBrains 공식 공급 기업입니다.
+                    당사는 제품에 대한 전문적인 지식을 바탕으로<br className='pc_tablet'/>
+                    고객들의 개발 환경, 고객이 당면한 문제점 등을 파악하여 
+                    그에 맞는 제품과 서비스를 합리적인 가격으로 제공합니다. 
+                </p>
+            </section>
+        </>
     );
 }
 
