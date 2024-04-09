2f4e2d8463205fe123628e1c14a2fac47e0ae9aa
김성은 <kse012050@naver.com>
2024-04-09 17:51:51 +0900

    제품 상세페이지 - 구매 옵션 완료

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { iconList } from '../js/product'
+import { Link } from 'react-router-dom';
 
 export default function ProductDetail() {
     return (
@@ -35,6 +36,70 @@ export default function ProductDetail() {
                         </div>
                     </li>
                 </ul>
+            </section>
+
+            <section className='purchaseArea'>
+                <h3>구매 옵션</h3>
+                <div>
+                    <b><span>상업용</span></b>
+                    <ul>
+                        <li title='신규 구매'>
+                            <span>
+                                $599<br/>
+                                (VAT 포함가 $658.90)
+                            </span>
+                        </li>
+                        <li title='2년차 갱신'>
+                            <span>
+                                $479<br/>
+                                (VAT 포함가 $526.90)
+                            </span>
+                        </li>
+                        <li title='3년차 이상 갱신'>
+                            <span>
+                                $359<br/>
+                                (VAT 포함가 $394.90)
+                            </span>
+                        </li>
+                        <li title='모든 기능'><span title='포함'></span></li>
+                        <li title='폴백 라이선스'><span title='포함'></span></li>
+                        <li title='라이선스 공유'><span title='포함'></span></li>
+                        <li title='기술 문의'><span title='포함'></span></li>
+                        <li title='라이선스 소유'><span>법인</span></li>
+                        <li><span><Link to='' className='btn-border-black'>견적요청</Link></span></li>
+                        <li><span><Link to='' className='btn-bg'>구매하기</Link></span></li>
+                    </ul>
+                </div>
+                <div>
+                    <b><span>개인용개인용</span></b>
+                    <ul>
+                        <li title='신규 구매'>
+                            <span>
+                                $169<br/>
+                                (VAT 포함가 $185.90)
+                            </span>
+                        </li>
+                        <li title='2년차 갱신'>
+                            <span>
+                                $135<br/>
+                                (VAT 포함가 $148.50)
+                            </span>
+                        </li>
+                        <li title='3년차 이상 갱신'>
+                            <span>
+                                $101<br/>
+                                (VAT 포함가 $111.10)
+                            </span>
+                        </li>
+                        <li title='모든 기능'><span title='포함'></span></li>
+                        <li title='폴백 라이선스'><span title='포함'></span></li>
+                        <li title='라이선스 공유'><span></span></li>
+                        <li title='기술 문의'><span></span></li>
+                        <li title='라이선스 소유'><span>개인</span></li>
+                        <li><span><Link to='' className='btn-border-black'>견적요청</Link></span></li>
+                        <li><span><Link to='' className='btn-bg'>구매하기</Link></span></li>
+                    </ul>
+                </div>
             </section>
         </>
     );
