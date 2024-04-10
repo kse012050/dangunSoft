0b0f48934923a079fafe09f6ccb28db088afb74f
김성은 <kse012050@naver.com>
2024-04-10 18:53:04 +0900

    제품 상세 페이지 임시 완료

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -109,6 +109,61 @@ export default function ProductDetail() {
                 <strong>개인용</strong>
                 <p>개인의 자금으로 Toolbox(JetBrains 데스크탑 개발자 도구) 구독을 구매하려는 경우, 개인용 Toolbox 구독이 적합합니다. 개인용 Toolbox 구독은 어떤 방식으로든 회사가 구매 및 상환할 수 없습니다. 개인용 Toolbox 구독을 통해 얻은 소프트웨어는 회사 하드웨어를 포함한 모든 하드웨어에서 사용할 수 있습니다.</p>
             </section>
+
+            <section className='FAQBox'>
+                <h3>FAQ</h3>
+                <ul>
+                    <li>
+                        <details>
+                            <summary>JetBrains의 라이선스 모델은 무엇인가요?</summary>
+                            <div>
+                                내용
+                            </div>
+                        </details>
+                    </li>
+                    <li>
+                        <details>
+                            <summary>JetBrains Toolbox란 무엇인가요?</summary>
+                            <div>
+                                <p>JetBrains Toolbox는 JetBrains 데스크탑 개발자 도구입니다. JetBrains Toolbox는 월간 또는 연간 구독으로 사용할 수 있으며 다음 제품들을 포함합니다.</p>
+                                <ul>
+                                    <li>CLion</li>
+                                    <li>DataGrip</li>
+                                    <li>dotCover</li>
+                                    <li>dotMemory</li>
+                                    <li>dotTrace</li>
+                                    <li>DataSpell</li>
+                                    <li>GoLand</li>
+                                    <li>IntelliJ IDEA</li>
+                                    <li>PhpStorm</li>
+                                    <li>PyCharm</li>
+                                    <li>ReSharper</li>
+                                    <li>ReSharper C++</li>
+                                    <li>Rider</li>
+                                    <li>RubyMine</li>
+                                    <li>WebStorm</li>
+                                </ul>
+                            </div>
+                        </details>
+                    </li>
+                    <li>
+                        <details>
+                            <summary>영구 폴백 라이선스란 무엇인가요?</summary>
+                            <div>
+                                <p>영구 폴백 라이선스는 JetBrains Toolbox(JetBrains 데스크탑 개발자 도구) 제품을 1년 간 구독했을때 제공되는 라이선스입니다. 영구 폴백 라이선스를 통해 특정 버전의 제품을 영구적으로 사용할 수 있습니다. JetBrains Toolbox 제품의 연간 구독을 구매하면 구매 시점에 최신 버전으로 영구 폴백 라이선스가 제공됩니다.</p>
+                            </div>
+                        </details>
+                    </li>
+                    <li>
+                        <details>
+                            <summary>여러 사용자가 동일한 상업용 라이선스를 사용할 수 있나요?</summary>
+                            <div>
+                                <p>여러 사용자가 동시에 하나의 라이선스를 사용할 수 없습니다. 만약 두 명의 사용자가 동시에 제품을 사용해야 한다면 사용자는 각자의 라이선스를 보유해야 합니다. 하지만 동시에 제품을 사용하지 않는 경우라면 여러 사용자가 하나의 라이선스를 공유할 수 있습니다.</p>
+                            </div>
+                        </details>
+                    </li>
+                </ul>
+            </section>
         </>
     );
 }
