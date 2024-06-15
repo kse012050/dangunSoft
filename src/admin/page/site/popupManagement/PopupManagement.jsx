cfed98bb3abaddb5b75020275500905191dc4ddd
김성은 <kse012050@naver.com>
2024-06-15 15:38:36 +0900

    관리자 - 팝업 삭제 완료

--- src/admin/page/site/popupManagement/PopupManagement.jsx
+++ src/admin/page/site/popupManagement/PopupManagement.jsx
@@ -1,12 +1,13 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { adminApi } from '../../../api/api';
+import Popup from '../../../components/popup/Popup';
 
 export default function PopupManagement() {
     const [board, setBoard] = useState()
+    const [popup, setPopup] = useState()
 
-
-    useLayoutEffect(()=>{
+    const boardFunc = useCallback(()=>{
         adminApi('board', '', {board_type: 'popup'})
             .then((result)=>{
                 // console.log(result);
@@ -16,17 +17,22 @@ export default function PopupManagement() {
                     })
                 }
             })
+
     },[])
 
-    const onExposure = (e, admin_id) => {
-        const { checked } = e.target
-        // adminApi('manage', 'update', {admin_id, super_admin_yn: checked ? 'y' : 'n'})
-        //     .then((result)=>{
-        //         // console.log(result);
-        //         if(result.result){
 
-        //         }
-        //     })
+    useLayoutEffect(()=>{
+        boardFunc()
+    },[boardFunc])
+
+    const onExposure = (e, data) => {
+        const { checked } = e.target
+        // "필수 파라미터가 존재하지 않습니다.(board_picture_ids,title,link,exposure_yn)"
+        adminApi('board/manage', 'update', {board_id: data.board_id, title: data.title, link: data.link, board_picture_ids: data.board_picture_ids, exposure_yn: checked ? 'y' : 'n'})
+            .then((result)=>{
+                if(result.result){
+                }
+            })
     }
 
     return (
@@ -64,18 +70,43 @@ export default function PopupManagement() {
                                 <span>{ data.link }</span>
                             </p>
                             <div className='exposure' onClick={(e)=>e.stopPropagation()}>
-                                <input type="checkbox" id={`check_${data.board_id}`} defaultChecked={data.exposure_yn === 'y'} onChange={(e)=>onExposure(e, data.admin_id)}/>
+                                <input type="checkbox" id={`check_${data.board_id}`} defaultChecked={data.exposure_yn === 'y'} onChange={(e)=>onExposure(e, data)}/>
                                 <label htmlFor={`check_${data.board_id}`}>노출 여부</label>
                             </div>
                             <time className='time'>{ data.reg_date }</time>
                             <div className='button'>
                                 <Link to={`/admin/site/popup/${data.board_id}`} className='btn-point'>수정</Link>
-                                <button className='btn-point-border'>삭제</button>
+                                <button className='btn-point-border'
+                                    onClick={()=>setPopup({
+                                        type: 'cancel', 
+                                        title: '알림',
+                                        description: [
+                                            '해당 계정를 삭제하겠습니까?',
+                                            '삭제된 정보는 복구할 수 없습니다.',
+                                        ],
+                                        func: () => {
+                                            adminApi('board/manage', 'delete', {board_id: data.board_id})
+                                                .then((result)=>{
+                                                    if(result.result){
+                                                        setPopup({
+                                                            type: 'confirm',
+                                                            title: '알림',
+                                                            description: ['해당 계정이 삭제 되었습니다.'],
+                                                            func: () =>{
+                                                                boardFunc()
+                                                            }
+                                                        })
+                                                    }
+                                                })
+                                        }
+                                    })}
+                                >삭제</button>
                             </div>
                         </li>
                     )}
                 </ol>
             </div>
+            { popup && <Popup popup={popup} setPopup={setPopup}/>}
         </>
     );
 }
